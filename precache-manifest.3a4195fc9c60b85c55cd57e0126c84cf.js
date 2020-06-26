182701a8f87868dac6e68b5ccce6e13a11697729
DCitalo <cd.italo@gmail.com>
2020-06-26 17:50:47 -0300

    Updates

---
+++ precache-manifest.3a4195fc9c60b85c55cd57e0126c84cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b343ab7c6260af4ddbb1565a719ab4f",
+    "url": "/PasteldeIdeias-Doc88/index.html"
+  },
+  {
+    "revision": "5cf3db9cd8affa93130a",
+    "url": "/PasteldeIdeias-Doc88/static/js/2.8e79cd4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PasteldeIdeias-Doc88/static/js/2.8e79cd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5750169241689d7dc5d",
+    "url": "/PasteldeIdeias-Doc88/static/js/main.f8c83cdd.chunk.js"
+  },
+  {
+    "revision": "8c52f7fef9c3928b01cd",
+    "url": "/PasteldeIdeias-Doc88/static/js/runtime-main.6af785a3.js"
+  },
+  {
+    "revision": "05c93ffb52c82ca49b88683427b2335e",
+    "url": "/PasteldeIdeias-Doc88/static/media/Logo.05c93ffb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b304a0dfbf917046cfd39cca8445d381.js"
+  "/PasteldeIdeias-Doc88/precache-manifest.3a4195fc9c60b85c55cd57e0126c84cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PasteldeIdeias-Doc88/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
